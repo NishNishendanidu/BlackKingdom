eb2143c3260f47c2950d1ef813d1202eeaac9288
Shen bro <96036613+NishNishendanidu@users.noreply.github.com>
2022-03-03 22:57:56 +0530

    Kingamda-X ⚡⚡

--- whatsasena/StringSession.js
+++ whatsasena/StringSession.js
@@ -6,7 +6,7 @@ you may not use this file except in compliance with the License.
 Blackkingdom - BlackKingdoms
 */
 
-const fs = require('fs');
+const fs = require('fs'); 
 
 class StringSession {
     constructor() {
